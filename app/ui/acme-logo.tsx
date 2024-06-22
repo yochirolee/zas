6422969887e63467e975c2f67a44490c03003574
yosho <yleecruz@gmail.com>
2024-06-22 13:28:39 -0400

    init

---
+++ app/ui/acme-logo.tsx
@@ -0,0 +1,13 @@
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+
+export default function AcmeLogo() {
+  return (
+    <div
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    >
+      <Image src="/zaslogo.png" alt="Acme Logo" width={48} height={48} />
+    </div>
+  );
+}
