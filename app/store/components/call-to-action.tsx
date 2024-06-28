6aa594615074dedde775c9035ad1b09106d20388
yosho <yleecruz@gmail.com>
2024-06-28 13:49:34 -0400

    shadcn added 4hiddencontroel

--- app/store/components/call-to-action.tsx
+++ app/store/components/call-to-action.tsx
@@ -50,8 +50,10 @@ export default function CallToAction() {
                   />
                 </CarouselItem>
               </CarouselContent>
-              <CarouselPrevious />
-              <CarouselNext />
+              <div className="hidden md:block">
+                <CarouselPrevious />
+                <CarouselNext />
+              </div>
               <div className="mt-6 flex w-full justify-center gap-4 ">
                 <div className="h-2 w-4 rounded-full   bg-gray-700 shadow-md"></div>
                 <div className="h-2 w-2 rounded-full bg-gray-600"></div>
