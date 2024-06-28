908a5ddcea650f3d79be0eb4b3161b079d3b6086
yosho <yleecruz@gmail.com>
2024-06-27 20:42:42 -0400

    shadcn added 2

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
-import logo from '@/public/zaslogo.png';
+import logo from '@/public/logo.svg';
 import Image from 'next/image';
 import {
   Card,
@@ -16,17 +16,15 @@ export default function SideNav() {
   return (
     <div className="flex h-full max-h-screen flex-col gap-2">
       <div className="flex h-14 items-center justify-center border-b px-4 lg:h-[60px] lg:px-6">
-        <Link
-          href="/dashboard"
-          className="flex items-center gap-2 font-semibold"
-        >
+        <Link href="/dashboard" className=" flex gap-2 font-semibold">
           <Image
             src={logo}
-            width={20}
-            height={20}
+            width={24}
+            height={24}
             alt="logo"
-            className="h-20 w-20 rotate-[16deg]"
+            className="h-24 w-24 rotate-[16deg] "
           />
+         
         </Link>
       </div>
       <div className="min-h-40 bg-gray-100">
@@ -37,7 +35,7 @@ export default function SideNav() {
         </div>
       </div>
       <NavLinks />
-    {/*   <div className="mt-auto p-4">
+      {/*   <div className="mt-auto p-4">
         <Card x-chunk="dashboard-02-chunk-0">
           <CardHeader className="p-2 pt-0 md:p-4">
             <CardTitle>Upgrade to Pro</CardTitle>
